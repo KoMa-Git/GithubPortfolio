5146902c5821e03ed4efa4ce5835855eacd3e4ba
KoMa-Git <matja82@gmail.com>
2025-07-21 01:02:18 +0200

    app.run() deploy version

--- Flask/minihome.py
+++ Flask/minihome.py
@@ -231,4 +231,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     # can use debug=True at development, then restart server automatically when save changes in code
-    app.run(debug=True)
+    app.run()
