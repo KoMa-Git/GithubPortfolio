e98a915d583e9891796afbc5867ebfb0b0d28adf
KoMa-Git <matja82@gmail.com>
2025-07-22 00:52:41 +0200

    make a test

--- Flask/test_app.py
+++ Flask/test_app.py
@@ -32,7 +32,7 @@ def login_user(client, email="test@example.com", password="test123"):
 
 def test_home_and_about_pages(client):
     assert client.get("/").status_code != 400
-    assert client.get("/about").status_code == 400
+    assert client.get("/about").status_code != 400
 
 def test_register(client):
     res = register_user(client)
